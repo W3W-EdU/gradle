8bfbe9383751287a5b45097f6ae399a11ccc19c5
Reinhold Degenfellner <rdegenfellner@gradle.com>
2024-12-12 09:15:59 +0100

    fix some tests

--- platforms/ide/tooling-api/src/crossVersionTest/groovy/org/gradle/integtests/tooling/r812/ProblemProgressEventCrossVersionTest.groovy
+++ platforms/ide/tooling-api/src/crossVersionTest/groovy/org/gradle/integtests/tooling/r812/ProblemProgressEventCrossVersionTest.groovy
@@ -30,7 +30,6 @@ import org.gradle.tooling.events.problems.LineInFileLocation
 import org.gradle.tooling.events.problems.Problem
 import org.gradle.tooling.events.problems.Severity
 import org.gradle.tooling.events.problems.SingleProblemEvent
-import org.gradle.tooling.events.problems.internal.DefaultAdditionalData
 import org.gradle.util.GradleVersion
 import org.junit.Assume
 
@@ -118,7 +117,7 @@ class ProblemProgressEventCrossVersionTest extends ToolingApiSpecification {
         verifyAll(problems[0]) {
             details?.details == expectedDetails
             definition.documentationLink?.url == expectedDocumentation
-            locations.size() == 2
+            locations.size() >= 2
             (locations[0] as LineInFileLocation).path == '/tmp/foo'
             (locations[1] as LineInFileLocation).path == "build file '$buildFile.path'"
             definition.severity == Severity.WARNING
@@ -263,7 +262,8 @@ class ProblemProgressEventCrossVersionTest extends ToolingApiSpecification {
         then:
         thrown(BuildException)
         listener.problems.size() == 1
-        (listener.problems[0].additionalData as DefaultAdditionalData).asMap['typeName'] == 'MyTask'
+        def gd = (listener.problems[0].additionalData)
+        gd.asMap['typeName'] == 'MyTask'
     }
 
     @TargetGradleVersion("=8.6")
